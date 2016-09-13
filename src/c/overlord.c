03b27a2a44fd744965c3e1a9b71cd7fec18db606
Grégoire Sage <gregoire.sage@gmail.com>
2016-09-13 14:48:29 +0200

    Update overlord.c

--- src/c/overlord.c
+++ src/c/overlord.c
@@ -17,9 +17,9 @@ uint16_t overlay_load(uint16_t overlay_id)
 		// Load the overlay
 		size_t size = resource_load_byte_range(
 			resource_get_handle(RESOURCE_ID_OVL_FULL), 					
-			_ovly_table[overlay_id][2] - _ovly_table[0][2], 			// offset in resource
-			(uint8_t *)(&__pbl_app_info) + _ovly_table[overlay_id][0], 	// load address, it should be &__pbl_app_info)+0x82+0x26
-			_ovly_table[overlay_id][1]);								// size of the overlay
+			_ovly_table[overlay_id][2] - _ovly_table[0][2],               // offset in resource
+			(uint8_t *)(&__pbl_app_info) + _ovly_table[overlay_id][0],    // load address, it should be &__pbl_app_info)+0x82+0x26
+			_ovly_table[overlay_id][1]);                                  // size of the overlay
 
 		// Check if the loaded size is correct
 		if(size == _ovly_table[overlay_id][1]){
